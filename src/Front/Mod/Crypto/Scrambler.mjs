8b80921ba05b0e71af262a52f85a6993c9dffce1
Alex Gusev <alex@flancer64.com>
2024-02-15 15:15:51 +0200

    pwa-ssk-test-96 Encrypt the portal messages
    link to issue: flancer32/pwa-ssk-test#96

--- src/Front/Mod/Crypto/Scrambler.mjs
+++ src/Front/Mod/Crypto/Scrambler.mjs
@@ -15,15 +15,43 @@ export default class Fl32_Auth_Front_Mod_Crypto_Scrambler {
         }
     ) {
         // VARS
+        let _keyShared;
+
         // FUNCS
-        const {sign} = nacl;
+        const {box, randomBytes, sign} = nacl;
 
         // INSTANCE METHODS
-        this.decryptAndVerify = function (encrypted) {};
+        this.decryptAndVerify = function (encrypted) {
+            let res;
+            const messageWithNonceAsUint8Array = util.b642ab(encrypted);
+            const nonce = messageWithNonceAsUint8Array.slice(0, box.nonceLength);
+            const message = messageWithNonceAsUint8Array.slice(
+                box.nonceLength,
+                encrypted.length
+            );
+            const decryptedAb = box.open.after(message, nonce, _keyShared);
+            if (decryptedAb) {
+                const jsonStr = util.ab2utf(decryptedAb);
+                res = JSON.parse(jsonStr);
+            }
+            return res;
+        };
 
-        this.encryptAndSign = function (plain) {};
+        this.encryptAndSign = function (plain) {
+            const messageUint8 = util.utf2ab(JSON.stringify(plain));
+            const nonce = randomBytes(box.nonceLength);
+            const encrypted = box.after(messageUint8, nonce, _keyShared);
+            const fullMessage = new Uint8Array(nonce.length + encrypted.length);
+            fullMessage.set(nonce);
+            fullMessage.set(encrypted, nonce.length);
+            return util.ab2b64(fullMessage);
+        };
 
-        this.setKeys = function (pub, sec) {};
+        this.setKeys = function (pub, sec) {
+            const abPub = util.b642ab(pub);
+            const abSec = util.b642ab(sec);
+            _keyShared = box.before(abPub, abSec);
+        };
 
         this.sign = function (message, sec) {
             const secret = util.b642ab(sec);
@@ -31,5 +59,9 @@ export default class Fl32_Auth_Front_Mod_Crypto_Scrambler {
             const signBin = sign(msgBin, secret);
             return util.ab2b64(signBin);
         };
+    }
+
+    verify(signed, pub) {
+        return undefined;
     }
 }
