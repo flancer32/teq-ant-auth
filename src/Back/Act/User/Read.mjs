dd29d8a54cbecdef53802ac60e535cab6cc5940a
Alex Gusev <alex@flancer64.com>
2024-04-03 12:34:36 +0300

    LP-27 Send the invitation link to an employee

--- src/Back/Act/User/Read.mjs
+++ src/Back/Act/User/Read.mjs
@@ -6,12 +6,14 @@
 export default class Fl32_Auth_Back_Act_User_Read {
     /**
      * @param {TeqFw_Db_Back_Api_RDb_CrudEngine} crud
+     * @param {Fl32_Auth_Back_RDb_Schema_Password} rdbPass
      * @param {Fl32_Auth_Back_RDb_Schema_User} rdbUser
      * @param {Fl32_Auth_Back_Convert_User} convUser
      */
     constructor(
         {
             TeqFw_Db_Back_Api_RDb_CrudEngine$: crud,
+            Fl32_Auth_Back_RDb_Schema_Password$: rdbPass,
             Fl32_Auth_Back_RDb_Schema_User$: rdbUser,
             Fl32_Auth_Back_Convert_User$: convUser,
         }
@@ -21,20 +23,38 @@ export default class Fl32_Auth_Back_Act_User_Read {
 
         // MAIN
         /**
-         * @param {TeqFw_Db_Back_RDb_ITrans} trx
-         * @param {string} [uuid]
-         * @return {Promise<{shared: Fl32_Auth_Shared_Dto_User.Dto, rdb: Fl32_Auth_Back_RDb_Schema_User.Dto}>}
+         * The structure of the returned value.
+         * @typedef {Object} ActResult
+         * @property {Fl32_Auth_Shared_Dto_User.Dto} shared
+         * @property {Fl32_Auth_Back_RDb_Schema_User.Dto} [rdb] @deprecated use `db` prefix
+         * @property {Fl32_Auth_Back_RDb_Schema_Password.Dto} [dbPass]
+         * @property {Fl32_Auth_Back_RDb_Schema_User.Dto} [dbUser]
+         * @memberof Fl32_Auth_Back_Act_User_Read
          */
-        this.act = async function ({trx, uuid}) {
+
+        /**
+         * @param {TeqFw_Db_Back_RDb_ITrans} trx
+         * @param {string} uuid
+         * @param {boolean} withPass
+         * @return {Promise<ActResult>}
+         */
+        this.act = async function ({trx, uuid, withPass}) {
+            /** @type {Fl32_Auth_Back_RDb_Schema_Password.Dto} */
+            let dbPass;
             /** @type {Fl32_Auth_Back_RDb_Schema_User.Dto} */
-            let rdb;
+            let dbUser;
             /** @type {Fl32_Auth_Shared_Dto_User.Dto} */
             let shared;
             if (uuid) {
-                rdb = await crud.readOne(trx, rdbUser, {[ATTR.UUID]: uuid});
-                if (rdb) shared = convUser.rdb2share(rdb);
+                dbUser = await crud.readOne(trx, rdbUser, {[ATTR.UUID]: uuid});
+                if (dbUser) {
+                    shared = convUser.rdb2share(dbUser);
+                    if (withPass) {
+                        dbPass = await crud.readOne(trx, rdbPass, dbUser.bid);
+                    }
+                }
             }
-            return {shared, rdb};
+            return {shared, rdb: dbUser, dbUser, dbPass};
         };
     }
 
